96b4f7ea5d19fcc750d1563b851baf1735e27881
Ivan Acuña <ivanacu007@outlook.com>
2021-09-02 01:03:18 -0700

    docs agregado

---
+++ docs/precache-manifest.bae00a13b9c7419cd2649eb271b93d05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3e8fe34d6e27d18076e0dc4091ca7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8421b99b92725c2b7fdd",
+    "url": "/static/css/main.ddb97c89.chunk.css"
+  },
+  {
+    "revision": "e107496e2e01402b74e7",
+    "url": "/static/js/2.b11609f6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.b11609f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8421b99b92725c2b7fdd",
+    "url": "/static/js/main.36ab0c2b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
